6e60179b55f3be90c7dc2dea99c457341a09d661
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2022-12-19 23:25:54 +0600

    first commit

---
+++ Es6_Concepts_For_React/logicalOperator.js
@@ -0,0 +1,13 @@
+
+const x = 5;
+
+//truthy = number, [],string,{},true
+
+//false = undefined, NaN, 0, false,null
+
+// const result = (x) && 'ami berate jabo';
+// console.log(result)
+
+
+const result = (x) || 'ami berate jabo';
+console.log(result)
